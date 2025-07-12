00da7140513a15cb1a31575e09ff20a58f60232f
Gabriel Sobral <gasobral@gmail.com>
2025-07-12 20:40:31 -0300

    Update custom_form.py
    Added information for each fruit.

--- custom_form.py
+++ custom_form.py
@@ -36,10 +36,11 @@ if ingredients_list:
     """
     time_to_insert = st.button('Submit Order')
 
-    for fruit_chosen in ingredients_list:
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+    for fruit_chosen in ingredients_list:
+        st.subhead(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
