3185a208e98d666da5826357c3eefd62a0facea6
Gabriel Sobral <gasobral@gmail.com>
2025-07-12 21:14:21 -0300

    Update custom_form.py
    Added pandas filter

--- custom_form.py
+++ custom_form.py
@@ -24,8 +24,9 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")\
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# check data loaded into pandas data frame
+# st.dataframe(pd_df)
+# st.stop()
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be ', name_on_order)
@@ -50,6 +51,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
     for fruit_chosen in ingredients_list:
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write(f'The search value for {fruit_chosen} is {search_on}.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
